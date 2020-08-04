ed604ea0a70c14714ed98dcecac6f1a1059987d0
Gustavo Silva <gstvds@icloud.com>
2020-08-04 00:38:59 -0300

    feat: Adds inputs to TeacherList

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -6,11 +6,13 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ children, title }) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -22,7 +24,9 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+      {children}
       </div>
+
     </header>
   )
 }
