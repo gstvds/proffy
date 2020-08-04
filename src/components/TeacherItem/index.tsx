dffd23c204b11878ffb507dd17110a9a7de40410
Gustavo Silva <gstvds@icloud.com>
2020-08-04 01:00:27 -0300

    refact: Moves TeacherItem from TeacherList to its component

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars0.githubusercontent.com/u/53450202?s=460&u=83aebabe6799b190292d00ad27e436b7543a2421&v=4" alt="Gustavo Silva" />
+        <div>
+          <strong>Gustavo Silva</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+            <br /><br />
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+          </p>
+      <footer>
+        <p>
+          Preço/hora
+              <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp" />
+              Entrar em contato
+            </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
-
 import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList() {
   return (
@@ -27,31 +26,9 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/53450202?s=460&u=83aebabe6799b190292d00ad27e436b7543a2421&v=4" alt="Gustavo Silva"/>
-            <div>
-              <strong>Gustavo Silva</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br/><br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="WhatsApp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   )
