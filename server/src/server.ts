cd721eac805a37a76cdf202d2684d372a1a187ee
Gustavo Silva <gstvds@icloud.com>
2020-08-04 21:34:54 -0300

    feat: Adds express and starts server with simple routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello World2')
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.get('/users', (request, response) =>
+  response.json({ message: 'Hello World' }),
+);
+
+app.listen(PORT, () => console.log(`🚀️ Servidor iniciado na porta ${PORT}`));
