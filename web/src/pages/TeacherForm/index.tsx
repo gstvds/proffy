ea6513d7b126a41fc1443b8a10f4ca22aab3bfe8
Gustavo Silva <gstvds@icloud.com>
2020-08-05 20:12:29 -0300

    feat: Adds available schedules styles

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -46,9 +46,36 @@ function TeacherForm() {
           <Input label="Custo da sua hora por aula" name="cost" type="text" />
         </fieldset>
 
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">
+              + Novo horário
+            </button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda-feira' },
+                { value: '2', label: 'Terça-feira' },
+                { value: '3', label: 'Quarta-feira' },
+                { value: '4', label: 'Quinta-feira' },
+                { value: '5', label: 'Sexta-feira' },
+                { value: '6', label: 'Sábado' },
+              ]}
+              label="Dia da semana"
+              name="week_day"
+            />
+            <Input name="from" label="De" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
+        </fieldset>
+
         <footer>
           <p>
-            <img src={warningIcon} alt="Aviso importante"/>
+            <img src={warningIcon} alt="Aviso importante" />
             Importante! <br />
             Preencha todos os dados
           </p>
