de62e29eb5808a84aa7249ddb22d7cda51d19161
Gustavo Silva <gstvds@icloud.com>
2020-08-04 23:09:46 -0300

    feat: Adds create connections and connections total list

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,12 +2,17 @@ import { Router } from 'express';
 import db from './database/connection';
 import convertHourToMinutes from './utils/convertHourToMinutes';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = Router();
 
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
+
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
