4a1687fcf2e8adfafaa9da0ed9bb620255fbf6d8
Gustavo Silva <gstvds@icloud.com>
2020-08-06 17:07:36 -0300

    feat: Adds assets and starts Landing Page

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Landing: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Landing Page</Text>
+    </View>
+  )
+};
+
+export default Landing;
