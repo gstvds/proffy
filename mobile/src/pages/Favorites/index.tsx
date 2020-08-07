18716ae6c4574473bdc6e5459bd9fb443aadab86
Gustavo Silva <gstvds@icloud.com>
2020-08-07 20:45:40 -0300

    feat: Adds favorites to Favorites page

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState<Teacher[]>([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites()
+  });
+
   return (
     <View style={styles.container}>
       <PageHeader
@@ -20,7 +37,13 @@ const Favorites: React.FC = () => {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
+        {favorites.map(teacher => (
+          <TeacherItem
+            key={teacher.id}
+            teacher={teacher}
+            favorited
+          />
+        ))}
       </ScrollView>
     </View>
   )
